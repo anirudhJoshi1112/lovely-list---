32ce4985926bb1095120ff59b627cea901ad9da9
Anirudh Joshi <120326069+anirudhJoshi1112@users.noreply.github.com>
2023-05-13 11:38:49 +0530

    basic syntax of a linked list

---
+++ ll1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* next; 
+
+    node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+};
+int main(){
+    node* node1=new node(10);
+    cout<<node1 -> data<<endl;
+    cout<<node1-> next <<endl;
+    node* head=node1;
+    return 0;
+}
